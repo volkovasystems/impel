478e812b774c691990664a1dd0861c2c9784ef4d
johnlenon <johnlenonmaghanoy@gmail.com>
2017-07-27 20:41:24 +0800

    Updated test.

--- impel-test.js
+++ impel-test.js
@@ -4,21 +4,21 @@ const impel = require( "./impel.js" );
 
 let test1 = { };
 impel( "hello", "world", test1 );
-assert.equal( test1.hello, "world", "should have value 'hello'" );
+assert.equal( test1.hello, "world", "should be equal to 'world'" );
 
 let descriptor = Object.getOwnPropertyDescriptor( test1, "hello" );
 
-assert.equal( descriptor.configurable, false, "should return false" );
+assert.equal( descriptor.configurable, false, "should be equal to false" );
 
-assert.equal( descriptor.enumerable, false, "should return false" );
+assert.equal( descriptor.enumerable, false, "should be equal to false" );
 
-assert.equal( descriptor.writable, false, "should return false" );
+assert.equal( descriptor.writable, false, "should be equal to false" );
 
 impel( "property", "value" );
-assert.equal( global.property, "value", "should have value" );
+assert.equal( global.property, "value", "should be equal to 'value'" );
 
 let test2 = { };
 impel( "property", "value", test2 )
-assert.equal( test2.property, "value", "should have value" );
+assert.equal( test2.property, "value", "should be equal to value" );
 
 console.log( "ok" );
